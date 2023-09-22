3438d156ceb1869a5c05274b5224655a5971e4db
Savaş Dede <svsdd22@gmail.com>
2023-09-22 16:22:40 +0300

    update

--- kotlin-api/application/src/main/kotlin/com/kotlin/api/controller/PersonController.kt
+++ kotlin-api/application/src/main/kotlin/com/kotlin/api/controller/PersonController.kt
@@ -1,20 +1,11 @@
 package com.kotlin.api.controller
 
-import ch.qos.logback.core.pattern.util.RegularEscapeUtil
 import com.kotlin.api.app.PersonService
 import com.kotlin.api.entity.Person
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
